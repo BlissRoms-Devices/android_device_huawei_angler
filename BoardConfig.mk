14e4868a83a5bca89a18e1b86dee81cf525ed162
electrikjesus <electrikjesus@gmail.com>
2016-09-27 08:37:55 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ USE_CLANG_PLATFORM_BUILD := true
 
 -include vendor/huawei/angler/BoardConfigVendor.mk
 
-BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
 # BLISS options
 TARGET_TC_ROM := 4.9-sm
