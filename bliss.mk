7d4ca12c06eb14a1ec756d2f3869be93b9cba164
blueyes <brian.w.petroski@gmail.com>
2016-10-12 17:04:43 -0500

    Update fingerprint to NRD90X

--- bliss.mk
+++ bliss.mk
@@ -39,6 +39,6 @@ PRODUCT_MANUFACTURER := Huawei
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90U/3155372:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90U 3155372 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90X/3254009:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90X 325009 release-keys"
 
