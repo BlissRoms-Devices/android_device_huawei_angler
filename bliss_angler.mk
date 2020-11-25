ca7344e772934fd485d907c799dc6d7df9f3afac
Hardik <hk912701@gmail.com>
2020-11-25 12:55:33 +0000

    update build FP from pixel 4 Nov

--- bliss_angler.mk
+++ bliss_angler.mk
@@ -41,7 +41,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
-BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys
 
 BLISS_BUILD_TYPE=UNOFFICIAL
 BLISS_BUILD_VARIANT=vanilla
